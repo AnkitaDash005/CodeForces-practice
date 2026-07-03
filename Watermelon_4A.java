f1e7314f36d1a980798557558835403ae77ebbc6
Ankita Dash <dashdeenesh098@gmail.com>
2026-07-03 20:10:46 +0530

    Initial Codeforces solutions

---
+++ Watermelon_4A.java
@@ -0,0 +1,16 @@
+//4A//
+import java.util.*;
+public class Watermelon_4A{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        int x=sc.nextInt();
+        if((x>2)&&(x%2==0)){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+        sc.close();
+    }
+
+} 
